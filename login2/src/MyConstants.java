883ba54f5a6ac1eaea25db2536d24ef40adc9e5a
joeycl1 <joeycl1@uci.edu>
2017-05-13 17:30:38 -0700

    add javax, recapcha library, recapcha verify utility + MyConstants

---
+++ login2/src/MyConstants.java
@@ -0,0 +1,9 @@
+class MyConstants {
+
+
+    public static final String SITE_KEY ="6Lf0OiEUAAAAACApcyMzFLdM9iZ-PZ4km_KY23tS";
+
+    public static final String SECRET_KEY ="6Lf0OiEUAAAAAJDhTFlrO5BNB-ZwO7gfJafETCp2";
+
+}
+
