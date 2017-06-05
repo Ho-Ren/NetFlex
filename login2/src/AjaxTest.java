e3d83d11c0d7a0982b6222555e4eeead062c26e3
joeycl1 <joeycl1@uci.edu>
2017-06-04 18:02:05 -0700

    pooling(added newer version of mysql-connector-java)

--- login2/src/AjaxTest.java
+++ login2/src/AjaxTest.java
@@ -1,5 +1,8 @@
 import java.io.*;
-
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.naming.Context;
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -10,10 +13,11 @@ import com.google.gson.Gson;
 
 public class AjaxTest extends HttpServlet {
 
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
+  @SuppressWarnings("unused")
+public void doGet(HttpServletRequest request, HttpServletResponse response)
                                throws ServletException, IOException {
-	
-//	Calendar c = Calendar.getInstance ();
+      PrintWriter out = response.getWriter();
+
 	String input = request.getParameter("Title");
 	System.out.println(input);
 	String[] token = input.split("\\s+");
@@ -21,28 +25,48 @@ public class AjaxTest extends HttpServlet {
 //    PrintWriter out = response.getWriter();
     ArrayList <String>list = new ArrayList<String> (); 
     
-//    out.println(c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND));
     try {
+    	 Context initCtx = new InitialContext();
+         if (initCtx == null)
+             out.println("initCtx is NULL");
+
+         Context envCtx = (Context) initCtx.lookup("java:comp/env");
+         if (envCtx == null)
+             out.println("envCtx is NULL");
+
+         // Look up our data source
+         DataSource ds = (DataSource) envCtx.lookup("jdbc/TestDB");
+         if (ds == null)
+             out.println("ds is null.");
+
+         Connection dbcon = ds.getConnection();
+         if (dbcon == null)
+             out.println("dbcon is null.");
+/*
     	Class.forName("com.mysql.jdbc.Driver").newInstance();
 		Connection dbcon = DriverManager.getConnection("jdbc:mysql:///moviedb?autoReconnect=true&useSSL=false", Globals.un, Globals.pw);
-		Statement st = dbcon.createStatement();
+*/		Statement st = dbcon.createStatement();
 //		System.out.println(input);
 		String query = "select title from movies where title ";
 		if(input.length()>3){
 			for(int i = 0; i <token.length; i++){
 				if (i == 0){
-					query+= "regexp '(^| )" + token[i]+"'";
+					query  += "regexp ?";
 				}
 				else
 				{
-					query += " and title regexp '(^| )" + token[i] +"'";
+					query += " and title regexp ?";
 				}
 				
 			}
 			
 		 System.out.println(query);
 		PreparedStatement stmt = dbcon.prepareStatement(query);
-		ResultSet rs =stmt.executeQuery();
+		for (int i = 0; i< token.length;i++){
+			stmt.setString(i+1,  "(^| )" + token[i]); 
+		}
+		 System.out.println(stmt.toString());
+		ResultSet rs = stmt.executeQuery();
 		while (rs.next()){
 			list.add(rs.getString(1));		
 //			System.out.println(rs.getString(1));
@@ -56,10 +80,10 @@ public class AjaxTest extends HttpServlet {
 		 response.setCharacterEncoding("UTF-8");
 		 response.getWriter().write(json);
 //		request.setAttribute("li", list);
-    } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+    } catch (SQLException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
-	} catch (SQLException e) {
+	} catch (NamingException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
